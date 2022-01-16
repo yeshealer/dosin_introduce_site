3c49d642b4c63d450c1b9f842e27c4aa1b2c1f64
dosin <m.john.incredible@gmail.com>
2022-01-16 16:15:27 +0100

    change template author

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 Name: 		Arter
 Version:  1.0
-Author:		Nazar Miller (millerDigitalDesign)
+Author:		Drago Jovic (millerDigitalDesign)
 Portfolio:  https://themeforest.net/user/millerdigitaldesign/portfolio?ref=MillerDigitalDesign
 
 p.s. I am available for Freelance hire (UI design, web development). mail: miller.themes@gmail.com
